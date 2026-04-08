0ad7fc620f114ae9e20f8ecd60fb447e70da7147
Ankita Mishra <avs.ankita30@gmail.com>
2026-04-08 08:37:35 -0500

    Fix text_splitter import for Streamlit Cloud

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from openai import OpenAI
